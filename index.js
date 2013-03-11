b56bc0a9f38fed0f5f24be113b7c81f61c14410b
Adriaa <dorgadria@gmail.com>
2013-03-12 00:37:09 +0100

    accents et reste

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
 	$("#exp_pro").click( function(){
 		var exp_pro = Handlebars.templates["exp_pro.html"]();
 		$("#content_cv").html(exp_pro);
+	});
+	$("#formations").click( function(){
+		var formations = Handlebars.templates["formations.html"]();
+		$("#content_cv").html(formations);
+	});	
+	$("#pro_inf").click( function(){
+		var pro_inf = Handlebars.templates["projets_info.html"]();
+		$("#content_cv").html(pro_inf);
 	});	
 });
